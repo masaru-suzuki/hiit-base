e2295f9a482a60e548dd37fff0d6a58ba6ecf213
masaru-suzuki <masaru.suzuki@socialplus.jp>
2025-01-11 12:31:44 +0900

    feat: user ページはユーザー情報を取得するようにする（途中）

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,20 +1,12 @@
-"use client";
-import { useExerciseTransactions } from "@/hooks/session/useExerciseTransactions";
+import { useUserData } from "@/hooks/polar/useUserData";
 
-export default function Page() {
-  const { exerciseTransactions, loading, error } = useExerciseTransactions();
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+export default async function Page() {
+  const data = await useUserData();
+  console.log(data);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <pre>{JSON.stringify(exerciseTransactions, null, 2)}</pre>
+      {data["polar-user-id"]}
     </div>
   );
 }
