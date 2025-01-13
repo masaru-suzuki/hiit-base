f7cb09d3af63cbf0fd3b97ae5f4ff6ae28e71919
masaru-suzuki <masaru.suzuki@socialplus.jp>
2025-01-13 15:20:11 +0900

    feat: user情報をプリント

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -2,11 +2,24 @@ import { useUserData } from "@/hooks/polar/useUserData";
 
 export default async function Page() {
   const data = await useUserData();
-  console.log(data);
+
+  const polarUserId = data["polar-user-id"];
+  const memberId = data["member-id"];
+  const firstName = data["first-name"];
+  const lastName = data["last-name"];
+  const weight = data.weight;
+  const height = data.height;
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {data["polar-user-id"]}
+      <p>
+        Polar User ID: {polarUserId}
+        <br />
+        Member ID: {memberId}
+        <br />
+        Hello, {lastName} {firstName} !<br />
+        体重は、{weight}kg、身長は{height}cmです。
+      </p>
     </div>
   );
 }
