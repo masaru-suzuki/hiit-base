6b1e785d78fd1a90d62863b2f90ac1b634e940ae
masaru-suzuki <masaru.suzuki@socialplus.jp>
2025-01-13 15:30:50 +0900

    feat: エクササイズ情報をプリント

---
+++ src/app/exercise/page.tsx
@@ -0,0 +1,20 @@
+import { useExercise } from "@/hooks/polar/useExercise";
+
+export default async function Page() {
+  const data = await useExercise();
+  console.log(data);
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>エクササイズ情報</h1>
+      {data.map((item) => {
+        return (
+          <div key={item.id}>
+            <p>{item.upload_time}</p>
+            <p>{item.sport}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
