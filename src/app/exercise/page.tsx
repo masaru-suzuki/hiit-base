b03a76ff6fc5c52c6526256e41a25286904f4040
masaru-suzuki <masaru.suzuki@socialplus.jp>
2025-01-13 16:53:46 +0900

    feat: 最大心拍数を追加

--- src/app/exercise/page.tsx
+++ src/app/exercise/page.tsx
@@ -2,7 +2,6 @@ import { useExercise } from "@/hooks/polar/useExercise";
 
 export default async function Page() {
   const data = await useExercise();
-  console.log(data);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -12,6 +11,7 @@ export default async function Page() {
           <div key={item.id}>
             <p>{item.upload_time}</p>
             <p>{item.sport}</p>
+            <p>{item.heart_rate.maximum}</p>
           </div>
         );
       })}
